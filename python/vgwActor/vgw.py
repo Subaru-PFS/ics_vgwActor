fadfca211adbdc69c047633da0eea95140992c06
Craig Loomis <cloomis@astro.princeton.edu>
2025-03-20 16:24:54 -1000

    modernize actor config handling

--- python/vgwActor/vgw.py
+++ python/vgwActor/vgw.py
@@ -2,19 +2,21 @@ import os
 from vgwActor.export import export
 from vgwActor.data_sink import DataSink
 
-
 class Vgw:
 
     def __init__(self, actor=None, logger=None):
 
         self.actor = actor
         self.logger = logger
-        confpath = os.path.expandvars(actor.config.get('data_sink', 'confpath'))
-        hostname = actor.config.get('data_sink', 'hostname')
-        username = actor.config.get('data_sink', 'username')
-        topic = actor.config.get('data_sink', 'topic')
-        self.rdatadir = os.path.expandvars(actor.config.get('data_sink', 'datadir'))
-        self.datadir = os.path.expandvars(actor.config.get(actor.name, 'datadir'))
+
+        config = actor.actorConfig
+        sinkConfig = config['data_sink']
+        confpath = os.path.expandvars(sinkConfig['confpath'])
+        hostname = sinkConfig['hostname']
+        username = sinkConfig['username']
+        topic = sinkConfig['topic']
+        self.rdatadir = os.path.expandvars(sinkConfig['datadir'])
+        self.datadir = os.path.expandvars(config['datadir'])
         self.data_sink = DataSink(confpath=confpath, hostname=hostname, username=username, topic=topic)
 
     def sendStatusKeys(self, cmd):
